eec13a101dd65f13c7951c3b88b6f0cce29d42bb
erza-503 <rendymahardika2007@gmail.com>
2025-01-02 19:27:26 +0800

    memperbarui navbar-1 ⬆️

--- Content/src/Components/Navbar.jsx
+++ Content/src/Components/Navbar.jsx
@@ -3,13 +3,32 @@ import logo from  "../assets/Pinterest-logo.png"
 const Navbar = () => {
 
     return (
-      <div>
-        <img src={logo} alt="Pinterest Logo" />
-        <p>Beranda</p>
-        <p>Jelajahi</p>
-        <div className="flex flex-row gap-3 bg-slate-400 text-gray-100 w-max py-2 px-4 rounded-xl">
-            <span className="magnifer" />
-            <input type="text" placeholder="Search something" className="bg-transparent placeholder:text-white focus:outline-none"/>
+      <div className="flex w-full justify-between align-middle items-center text-center">
+        <img src={logo} alt="Pinterest Logo" className="w-6 m-2" />
+        <div>
+          <ul className="flex flex-row gap-3 ">
+            <li>
+              <a href="">Beranda</a>
+            </li>
+            <li>
+              <a href="">Jelajahi</a>
+            </li>
+            <li>
+              <a href="">Buat</a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-row gap-3 bg-slate-200 text-gray-500 w-max py-2 px-4 rounded-xl">
+          <span className="magnifer" />
+          <input
+            type="text"
+            placeholder="Search something"
+            className="bg-transparent placeholder: text-gray-500 focus:outline-none"
+          />
+        </div>
+        <div className=" text-gray-500">
+          <span className="bell"></span>
+          <span className="chat"></span>
         </div>
       </div>
     );

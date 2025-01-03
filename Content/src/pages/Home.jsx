cfb7f00f9998ce94e804759f23b71fe0408e3d83
erza-503 <rendymahardika2007@gmail.com>
2025-01-03 22:25:11 +0800

    add font

--- Content/src/pages/Home.jsx
+++ Content/src/pages/Home.jsx
@@ -4,6 +4,11 @@ const Home = () => {
     return(
         <div>
             <Navbar />
+            <div className="ml-4">
+                {/* <ul>
+                    <li><a href="#" className="font-bold text-lg underline">semua</a></li>
+                </ul> */}
+            </div>
         </div>
     )
 }
--- Content/tailwind.config.js
+++ Content/tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { "Noto-sans": ["Noto-sans", "sans-serif"] },
+    },
   },
   plugins: [],
 };
